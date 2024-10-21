9bb8bd3cb0470c44cf8c98da355a1c657f2e7e14
Deepanshu Rohilla <20001001027deepanshu@dcrustm.org>
2024-10-21 22:10:17 +0530

    Add a button that lets the user generate a fresh mnemonic phrase

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import { useState } from "react";
 import './App.css'
+import { generateMnemonic } from "bip39";
 
 
 function App() {
   const[count, setCount] = useState(0);
 
-  const [mnemonic, setMnemonic] = useState("")
+  const [mnemonic, setMnemonic] = useState("");
+
 
   return (
     <>
-
+      <button onClick={async function(){
+        const mn = await generateMnemonic();
+        setMnemonic(mn);
+      }}>
+        Create Seed Phrase
+        </button>
     </>
   )
 }
